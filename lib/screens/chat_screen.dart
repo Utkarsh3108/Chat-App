124a974ceeea6f5ed39caad2dd7f62231bff3a39
Utkarsh3108 <56546503+Utkarsh3108@users.noreply.github.com>
2023-04-17 09:16:54 +0530

    Add files via upload

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,93 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../widgets/chat/messages.dart';
+import '../widgets/chat/new_message.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  // @override
+  // void initState() {
+  //   final fbm = FirebaseMessaging.instance;
+  //   fbm.requestPermission();
+  //   FirebaseMessaging.onMessage.listen((message) {
+  //     print(message);
+  //     return;
+  //   });
+  //   FirebaseMessaging.onBackgroundMessage((message) {
+  //     print(message);
+  //     return;
+  //   });
+  //   super.initState();
+  // }
+  @override
+  void initState() async {
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("Ypooooooooooooooooooooooo");
+      print(message.data);
+    });
+    Future<void> _firebaseMessagingBackgroundHandler(
+        RemoteMessage message) async {
+      await Firebase.initializeApp();
+      print(message.data);
+    }
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chatify'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage()
+          ],
+        ),
+      ),
+    );
+  }
+}
